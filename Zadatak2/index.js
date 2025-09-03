c00a53a560916ccb26974bdaa051e72169e9da29
eris00 <erissutkovic00@gmail.com>
2025-09-03 16:09:36 +0200

    bubnjevi task finished

--- Zadatak2/index.js
+++ Zadatak2/index.js
@@ -1,22 +1,68 @@
 import { bankOne, bankTwo } from './data.js';
 
-const buttons = document.querySelector('.buttons-section');
+const buttons = document.querySelector('.buttons-wrapper');
 const soundName = document.querySelector('.sound-name');
 const slider = document.querySelector('.slider');
 
-let dataOne = [...bankOne];
-dataOne.map((bank) => {
-  let buttonElement = document.createElement('button');
-  buttonElement.classList.add('button');
-  buttonElement.innerHTML = bank.keyTrigger;
-  let audio = new Audio(bank.url);
-  buttonElement.addEventListener('click', () => {
-    audio.play();
-    soundName.innerHTML = bank.id;
-  });
-  buttons.appendChild(buttonElement);
+let currentBank = bankOne;
+
+const audio = new Audio();
+
+let isPowerOn = true;
+
+let volume = 0.5;
+slider.addEventListener('input', (e) => {
+  volume = e.target.value;  
 });
 
-slider.addEventListener('input', (e) => {
-  audio.volume(e.value);
+const renderBtnBad = (currBank, vol, isPowerOn) => {
+  buttons.innerHTML = "";
+
+  currBank.forEach((bank) => {
+    let buttonElement = document.createElement('button');
+    buttonElement.classList.add('button');
+    buttonElement.innerHTML = bank.keyTrigger;
+
+    buttonElement.addEventListener('click', () => {
+      audio.src = bank.url;
+      audio.volume = vol;
+      soundName.innerHTML = bank.id;
+      if (isPowerOn) audio.play();
+    });
+    buttons.appendChild(buttonElement);
+  });
+}
+
+// initial render
+renderBtnBad(currentBank, volume, isPowerOn);
+
+// Power swtich
+const powerSwitcher = document.getElementById("power-switcher");
+powerSwitcher.classList.toggle("active");
+const powerSwitcherBtn = document.getElementById("power-switcher-btn");
+powerSwitcherBtn.addEventListener('click', () => {
+  powerSwitcher.classList.toggle("active");
+  isPowerOn = !isPowerOn;
+  renderBtnBad(currentBank, volume, isPowerOn);
 });
+
+// Bank swtich
+const bankSwitcher = document.getElementById("bank-switcher");
+const bankSwitcherBtn = document.getElementById("bank-swticher-btn");
+bankSwitcherBtn.addEventListener('click', () => {
+  bankSwitcher.classList.toggle("active");
+  if (currentBank === bankOne) {
+    currentBank = bankTwo;
+  } else if (currentBank === bankTwo) {
+    currentBank = bankOne;
+  } else {
+    currentBank = bankOne;
+  }
+  renderBtnBad(currentBank, volume, isPowerOn)
+  
+})
+
+
+
+
+
